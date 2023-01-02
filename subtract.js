185d4485ba80c381ca1dfa3b22d9ce9dffbcd38d
tauqeerahmed221 <tauqeerahmed221@gmail.com>
2023-01-02 17:38:26 +0530

    reset

---
+++ subtract.js
@@ -0,0 +1,7 @@
+function subtract()
+{
+var a = document.getElementById("firstno").value
+var b = document.getElementById("secondno").value
+var result
+document.getElementById("result").value = result
+}
